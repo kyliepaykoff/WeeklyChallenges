1babc0f0ba621fe8ca93a992a219abca8eb769d0
Kylie Paykoff <kyliepaykoff@gmail.com>
2024-05-17 14:19:58 -0400

    initial commit

---
+++ ChallengesWithTestsMark8/ChallengesSet01Helpers.cs
@@ -0,0 +1,14 @@
+﻿internal static class ChallengesSet01Helpers
+{
+    public static bool AreTwoNumbersTheSame(int num1, int num2)
+    {
+        if (num1 == num2)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
--- ChallengesWithTestsMark8/Program.cs
+++ ChallengesWithTestsMark8/Program.cs
@@ -1,12 +1,1 @@
-﻿using System;
-
-namespace ChallengesWithTestsMark8
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, world!");
-        }
-    }
-}
+﻿
